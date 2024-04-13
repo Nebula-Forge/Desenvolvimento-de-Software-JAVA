93f94727b7cec9f355722351d4d0b5a762987a74
DannyahIA <danispeed2013@gmail.com>
2024-04-12 21:35:27 -0300

    Exercicio 01

---
+++ LE01/src/br/edu/Programa.java
@@ -0,0 +1,49 @@
+package br.edu.up;
+
+import br.edu.up.exercicios.model.Exercicio01;
+
+public class Programa {
+
+    public static void main(String[] args) {
+
+        Exercicio01.ReceberNumero();
+
+        // Exercicio02.
+
+        // Exercicio03.
+
+        // Exercicio04.
+
+        // Exercicio05.
+
+        // Exercicio06.
+
+        // Exercicio07.
+
+        // Exercicio08.
+
+        // Exercicio09.
+
+        // Exercicio10.
+        
+        // Exercicio11.
+
+        // Exercicio12.
+
+        // Exercicio13.
+
+        // Exercicio14.
+
+        // Exercicio15.
+
+        // Exercicio16.
+
+        // Exercicio17.
+
+        // Exercicio18.
+
+        // Exercicio19.
+
+        // Exercicio20.
+    }
+}
---
+++ LE01/src/br/edu/up/model/Exercicio01.java
@@ -0,0 +1,26 @@
+package br.edu.up.exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    // 1. Escreva um algoritmo que leia um número digitado pelo usuário e mostre
+    // amensagem “Número maior do que 10!”, caso este número seja maior, ou
+    // “Númeromenor ou igual a 10!”, caso este número seja menor ou igual.
+
+    public static void ReceberNumero() {
+
+        PrintController.ExibirNaTela(01);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+
+        int valor = scanner.nextInt();
+
+        if (valor > 10) {
+            System.out.println("\nNúmero maior do que 10!");
+        } else {
+            System.out.println("\nNúmero menor ou igual a 10!");
+        }
+    }
+}
