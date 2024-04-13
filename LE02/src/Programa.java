d5eeaded8ceafbb14955765266fbf83c0b9b8627
DannyahIA <danispeed2013@gmail.com>
2024-04-13 09:56:59 -0300

    Exercicio 01 completo

--- LE02/src/Programa.java
+++ LE02/src/Programa.java
@@ -1,5 +1,64 @@
+import br.edu.up.controller.*;
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = ScannerUtil.getScanner();
+
+        System.out.println("Escolha um exercício de 1 a 15:");
+
+        int escolha = scanner.nextInt();
+
+        switch (escolha) {
+            case 1:
+                Exericio01.CalculoMedia();
+                break;
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+
+                break;
+            case 7:
+
+                break;
+            case 8:
+
+                break;
+            case 9:
+
+                break;
+            case 10:
+
+                break;
+            case 11:
+
+                break;
+            case 12:
+
+                break;
+            case 13:
+
+                break;
+            case 14:
+
+                break;
+            case 15:
+
+                break;
+            default:
+                ScannerUtil.closeScanner();
+                System.exit(0);
+        }
+
     }
 }
---
+++ LE02/src/br/edu/up/controller/Exericio01.java
@@ -0,0 +1,47 @@
+package br.edu.up.controller;
+
+import java.util.Scanner;
+
+public class Exericio01 {
+    // 1. Construa um programa que leia em um vetor uma sequência de 5 números digitados pelo usuário, 
+    // calcule a média e apresente na tela quais valores são menores, iguais ou superiores à média.
+
+    public static void CalculoMedia(){
+        Scanner scanner = ScannerUtil.getScanner();
+        double[] numeros = new double[5];
+        double soma = 0;
+
+        // Lendo os números digitados pelo usuário e armazenando no vetor
+        System.out.println("Digite 5 números:");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = scanner.nextDouble();
+            soma += numeros[i];
+        }
+
+        // Calculando a média
+        double media = soma / numeros.length;
+        System.out.println("A média é: " + media);
+
+        // Verificando quais valores são menores, iguais ou superiores à média
+        System.out.println("Valores menores que a média:");
+        for (double numero : numeros) {
+            if (numero < media) {
+                System.out.println(numero);
+            }
+        }
+
+        System.out.println("Valores iguais à média:");
+        for (double numero : numeros) {
+            if (numero == media) {
+                System.out.println(numero);
+            }
+        }
+
+        System.out.println("Valores superiores à média:");
+        for (double numero : numeros) {
+            if (numero > media) {
+                System.out.println(numero);
+            }
+        } 
+    }
+}
