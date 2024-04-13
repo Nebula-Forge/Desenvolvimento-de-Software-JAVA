86e0017693000cf2ff0c4ccc89e54cefd4724435
DannyahIA <danispeed2013@gmail.com>
2024-04-12 21:49:13 -0300

    Exercicio02

--- LE01/src/Programa.java
+++ LE01/src/Programa.java
@@ -5,5 +5,7 @@ public class Programa {
     public static void main(String[] args) {
 
         Exercicio01.ReceberNumero();
+
+        Exercicio02.SomaNumeros();
     }
 }
---
+++ LE01/src/br/edu/up/controller/Exercicio02.java
@@ -0,0 +1,26 @@
+package br.edu.up.controller;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    // 2. Escreva um algoritmo que leia dois números digitados pelo usuário e exiba
+    // o resultado da sua soma.
+
+    public static void SomaNumeros() {
+
+        PrintController.ExibirNaTela(02);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+
+        int valor1 = scanner.nextInt();
+
+        System.out.println("Digite outro número: ");
+
+        int valor2 = scanner.nextInt();
+
+        System.out.println("\nO valor somado é de: " + (valor1 + valor2));
+    }
+}
+
